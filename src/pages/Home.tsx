a3679253edadc0a7999e4c80fd7596b2f41e132a
jhayvee <jhayveeatencia11@gmail.com>
2023-12-18 19:13:36 +0800

    implement api call and display random user list with delete function

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonAvatar, IonContent, IonHeader, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonList, IonPage, IonTitle, IonToolbar, useIonAlert } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 import { useEffect, useState } from 'react';
@@ -6,6 +6,7 @@ import { fetchUserList } from '../api/users';
 
 const Home: React.FC = () => {
   const [users, setUsers] = useState([]);
+  const [presentAlert] = useIonAlert();
 
   useEffect(() => {
     fetchUsers();
@@ -13,24 +14,70 @@ const Home: React.FC = () => {
 
   const fetchUsers = async () => {
     const response: any = await fetchUserList();
-    console.log(response);
-    setUsers(response.result)
+    console.log(response.data);
+    setUsers(response.data.results)
+  }
+
+  const removeUser = (user: any) => {
+    setUsers([...users.filter((item: any) => item?.id?.value != user?.id?.value)]);
+  }
+
+  const showConfirmRemoveUser = (user: any) => {
+    presentAlert({
+      header: 'Remove User',
+      message: `Are you sure you want to remove ${user?.name?.first} ${user?.name?.last} from list?`,
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Confirm',
+          handler: () => removeUser(user)
+        }
+      ],
+    })
+  }
+
+  const UserItem = ({ user }: any) => {
+    return (
+      <IonItemSliding>
+        <IonItem>
+          <IonAvatar slot='start'>
+            <img src={user?.picture?.thumbnail} alt="" />
+          </IonAvatar>
+          <IonLabel>
+            {user?.name?.first} {user?.name?.last}
+          </IonLabel>
+        </IonItem>
+
+        <IonItemOptions>
+          <IonItemOption color="danger" onClick={showConfirmRemoveUser}>Delete</IonItemOption>
+        </IonItemOptions>
+      </IonItemSliding>
+    )
   }
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Random Users</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Random Users</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <IonList>
+          {
+            users.map((user: any, index: number) => {
+              return (<UserItem user={user} key={`userItemComp${user?.id?.name}${index}`} />)
+            })
+          }
+        </IonList>
       </IonContent>
     </IonPage>
   );
